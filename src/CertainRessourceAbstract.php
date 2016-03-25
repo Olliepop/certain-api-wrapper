3ad32dae877eed58da88d5254feceac26b9885fa
Raphaël BERGINA <raphael.bergina@wabel.com>
2016-03-25 14:47:41 +0100

    get size, max result, completesizeresult

--- src/CertainRessourceAbstract.php
+++ src/CertainRessourceAbstract.php
@@ -1,25 +1,26 @@
 <?php
+
 namespace Wabel\CertainAPI;
 
 use Wabel\CertainAPI\Interfaces\CertainRessourceInterface;
 use Wabel\CertainAPI\Interfaces\CertainResponseInterface;
-
 /**
  * CertainRessourceAbstracct for common action about Ressource
  *
  * @author rbergina
  */
-
 use Wabel\CertainAPI\CertainApiService;
 
 abstract class CertainRessourceAbstract implements CertainRessourceInterface, CertainResponseInterface
 {
     const NOT_FOUND = 404;
+
     /**
      * CertainApiService
      * @var CertainApiService
      */
     protected $certainApiService;
+
     /**
      * array of results with information about the request
      * @var array
@@ -32,7 +33,6 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
      */
     protected $ressourceCalled;
 
-
     /**
      * @param CertainApiService $certainApiService
      */
@@ -40,7 +40,6 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
     {
         $this->certainApiService = $certainApiService;
     }
-
 
     /**
      * Get information about ressource
@@ -51,12 +50,16 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
      * @return \Wabel\CertainAPI\CertainRessourceAbstract
      * @throws Exceptions\RessourceException
      */
-    public function get($ressourceId= null,$params= array(), $assoc = false, $contentType='json'){
-        $ressourceName = $this->getRessourceName();;
-        if($ressourceName == '' || is_null($ressourceName)){
+    public function get($ressourceId = null, $params = array(), $assoc = false,
+                        $contentType = 'json')
+    {
+        $ressourceName = $this->getRessourceName();
+        ;
+        if ($ressourceName == '' || is_null($ressourceName)) {
             throw new Exceptions\RessourceException('No ressource name provided.');
         }
-        $this->results = $this->certainApiService->get($ressourceName,$this->ressourceCalled, $ressourceId,$params, $assoc, $contentType);
+        $this->results = $this->certainApiService->get($ressourceName,
+            $this->ressourceCalled, $ressourceId, $params, $assoc, $contentType);
         return $this;
     }
 
@@ -71,23 +74,29 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
      * @throws Exceptions\RessourceException
      * @throws Exceptions\RessourceMandatoryFieldException
      */
-    public function post($bodyData, $query=array(), $ressourceId= null, $assoc = false, $contentType='json'){
-        $ressourceName = $this->getRessourceName();;
-        if($ressourceName == '' || is_null($ressourceName)){
+    public function post($bodyData, $query = array(), $ressourceId = null,
+                         $assoc = false, $contentType = 'json')
+    {
+        $ressourceName = $this->getRessourceName();
+        ;
+        if ($ressourceName == '' || is_null($ressourceName)) {
             throw new Exceptions\RessourceException('No ressource name provided.');
         }
-        if($ressourceId === null && count($this->getMandatoryFields())>0){
+        if ($ressourceId === null && count($this->getMandatoryFields()) > 0) {
             foreach ($this->getMandatoryFields() as $field) {
-                if(!in_array($field,  array_keys($bodyData))){
-                    throw new Exceptions\RessourceMandatoryFieldException(sprintf('The field %s is required',$field));
+                if (!in_array($field, array_keys($bodyData))) {
+                    throw new Exceptions\RessourceMandatoryFieldException(sprintf('The field %s is required',
+                        $field));
                 }
             }
         }
-        $this->results = $this->certainApiService->post($ressourceName, $this->ressourceCalled, $ressourceId, $bodyData, $query, $assoc, $contentType);
+        $this->results = $this->certainApiService->post($ressourceName,
+            $this->ressourceCalled, $ressourceId, $bodyData, $query, $assoc,
+            $contentType);
         return $this;
     }
 
-   /**
+    /**
      * Update information to certain
      * @param array $bodyData
      * @param array $query
@@ -98,21 +107,27 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
      * @throws Exceptions\RessourceException
      * @throws Exceptions\RessourceMandatoryFieldException
      */
-    public function put($bodyData, $query=array(), $ressourceId= null, $assoc = false, $contentType='json'){
-        $ressourceName = $this->getRessourceName();;
-        if($ressourceName == '' || is_null($ressourceName)){
+    public function put($bodyData, $query = array(), $ressourceId = null,
+                        $assoc = false, $contentType = 'json')
+    {
+        $ressourceName = $this->getRessourceName();
+        ;
+        if ($ressourceName == '' || is_null($ressourceName)) {
             throw new Exceptions\RessourceException('No ressource name provided.');
         }
-        if(!is_null($ressourceId) && count($this->getMandatoryFields())>0){
+        if (!is_null($ressourceId) && count($this->getMandatoryFields()) > 0) {
             foreach ($this->getMandatoryFields() as $field) {
-                if(!in_array($field,  array_keys($bodyData))){
-                    throw new Exceptions\RessourceMandatoryFieldException(sprintf('The field %s is required',$field));
+                if (!in_array($field, array_keys($bodyData))) {
+                    throw new Exceptions\RessourceMandatoryFieldException(sprintf('The field %s is required',
+                        $field));
                 }
             }
-        }else{
+        } else {
             throw new Exceptions\RessourceMandatoryFieldException(sprintf('The id field is required'));
         }
-        $this->results = $this->certainApiService->put($ressourceName, $this->ressourceCalled, $ressourceId, $bodyData, $query, $assoc, $contentType);
+        $this->results = $this->certainApiService->put($ressourceName,
+            $this->ressourceCalled, $ressourceId, $bodyData, $query, $assoc,
+            $contentType);
         return $this;
     }
 
@@ -124,12 +139,15 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
      * @return \Wabel\CertainAPI\CertainRessourceAbstract
      * @throws Exceptions\RessourceException
      */
-    public function delete($ressourceId, $assoc = false, $contentType='json'){
-        $ressourceName = $this->getRessourceName();;
-        if($ressourceName == '' || is_null($ressourceName)){
+    public function delete($ressourceId, $assoc = false, $contentType = 'json')
+    {
+        $ressourceName = $this->getRessourceName();
+        ;
+        if ($ressourceName == '' || is_null($ressourceName)) {
             throw new Exceptions\RessourceException('No ressource name provided.');
         }
-        $this->results = $this->certainApiService->delete($ressourceName, $this->ressourceCalled, $ressourceId, $assoc, $contentType);
+        $this->results = $this->certainApiService->delete($ressourceName,
+            $this->ressourceCalled, $ressourceId, $assoc, $contentType);
         return $this;
     }
 
@@ -146,10 +164,12 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
      * Check is not found.
      * @return boolean
      */
-    public  function isNotFound(){
-        if(isset($this->results['statusCode']) && $this->results['statusCode'] == self::NOT_FOUND){
+    public function isNotFound()
+    {
+        if (isset($this->results['statusCode']) && $this->results['statusCode'] == self::NOT_FOUND) {
             return true;
-        } elseif (isset($this->results['statusCode']) && $this->results['statusCode'] != self::NOT_FOUND){
+        } elseif (isset($this->results['statusCode']) && $this->results['statusCode']
+            != self::NOT_FOUND) {
             return false;
         }
         return null;
@@ -168,7 +188,8 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
      * Get the succes value, results,  success or fail reason
      * @return array
      */
-    public function getCompleteResults(){
+    public function getCompleteResults()
+    {
         return $this->results;
     }
 
@@ -180,14 +201,13 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
     public function createRessourceCalled($ressourceCalledParameters = null)
     {
         $this->ressourceCalled = '';
-        if(is_array($ressourceCalledParameters) && !empty($ressourceCalledParameters)){
+        if (is_array($ressourceCalledParameters) && !empty($ressourceCalledParameters)) {
             foreach ($ressourceCalledParameters as $segmentKey => $segmentValue) {
-                if($segmentValue != ''){
+                if ($segmentValue != '') {
                     $this->ressourceCalled .= '/'.$segmentKey.'/'.$segmentValue;
-                }else{
+                } else {
                     $this->ressourceCalled .= '/'.$segmentKey;
                 }
-
             }
         }
 
@@ -203,7 +223,8 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
      * @param string $contentType
      * @return \Wabel\CertainAPI\CertainRessourceAbstract
      */
-    public function getWithEventCode($eventCode,$ressourceId,$params= array(), $assoc = false, $contentType='json')
+    public function getWithEventCode($eventCode, $ressourceId, $params = array(),
+                                     $assoc = false, $contentType = 'json')
     {
         $ressourceId = $eventCode.'/'.$ressourceId;
         return $this->get($ressourceId, $params, $assoc, $contentType);
@@ -219,7 +240,9 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
      * @param string $contentType
      * @return \Wabel\CertainAPI\CertainRessourceAbstract
      */
-    public function postWithEventCode($eventCode,$ressourceId,$bodyData, $query=array(),$assoc = false, $contentType='json')
+    public function postWithEventCode($eventCode, $ressourceId, $bodyData,
+                                      $query = array(), $assoc = false,
+                                      $contentType = 'json')
     {
         $ressourceId = $eventCode.'/'.$ressourceId;
         return $this->post($bodyData, $query, $ressourceId, $assoc, $contentType);
@@ -235,7 +258,9 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
      * @param string $contentType
      * @return \Wabel\CertainAPI\CertainRessourceAbstract
      */
-    public function putWithEventCode($eventCode,$ressourceId,$bodyData, $query=array(), $assoc = false, $contentType='json')
+    public function putWithEventCode($eventCode, $ressourceId, $bodyData,
+                                     $query = array(), $assoc = false,
+                                     $contentType = 'json')
     {
         $ressourceId = $eventCode.'/'.$ressourceId;
         return $this->put($bodyData, $query, $ressourceId, $assoc, $contentType);
@@ -249,11 +274,49 @@ abstract class CertainRessourceAbstract implements CertainRessourceInterface, Ce
      * @param string $contentType
      * @return \Wabel\CertainAPI\CertainRessourceAbstract
      */
-    public function deleteWithEventCode($eventCode,$ressourceId, $assoc = false, $contentType='json')
+    public function deleteWithEventCode($eventCode, $ressourceId,
+                                        $assoc = false, $contentType = 'json')
     {
         $ressourceId = $eventCode.'/'.$ressourceId;
         return $this->delete($ressourceId, $assoc, $contentType);
     }
 
+    /**
+     * Return the size.
+     * @return int
+     */
+    public function getSize()
+    {
+        $result = $this->getResults();
+        if (!isset($result->size)) {
+            return null;
+        }
+        return $result->size;
+    }
 
+    /**
+     * Return the completeCollectionSizet.
+     * @return int
+     */
+    public function getCompleteCollectionSize()
+    {
+        $result = $this->getResults();
+        if (!isset($result->size)) {
+            return null;
+        }
+        return $result->completeCollectionSize;
+    }
+
+    /**
+     * Return the maxResults.
+     * @return int
+     */
+    public function getMaxResults()
+    {
+        $result = $this->getResults();
+        if (!isset($result->size)) {
+            return null;
+        }
+        return $result->maxResults;
+    }
 }
